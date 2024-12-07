1bbb743d6b5b7aca3e03c19c7b2af2e9f97147ba
xtenas <dxtenas.gmail.com>
2024-12-07 14:33:10 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<int> list = new List<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        list.Add(4);
+
+        foreach (int i in list)
+        {
+            Console.WriteLine(i);
+        }
+
+
+
+
+    }
+}
